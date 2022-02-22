e5e39c3c924f397eca7b202aba059eed8f8b8968
Oficina Tecnica <Otecnica@sheelec.dom>
2022-02-22 16:13:18 -0300

    resolviendo problemas

--- app.js
+++ app.js
@@ -18,15 +18,6 @@ app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
 });
 
-<<<<<<< HEAD
 app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
 });
-=======
-app.listen(process.env.PORT || 3000, function(){
-    console.log('Servidor corriendo en el puerto 3000');
-});
-
-
-
->>>>>>> b5a17f3e3a9ba248e40529af9e63f364c52f864c
